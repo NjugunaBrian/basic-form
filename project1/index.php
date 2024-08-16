e7ec437cdf3c75187620538dc02ae8572604ad57
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-08-16 20:33:47 +0300

    feat: connect & add data to database

---
+++ project1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h3>SignUp</h3>
+
+<form action="includes/formHandler.inc.php" method="POST">
+    <input type="text" name="username" placeholder="Username"  />
+    <input type="text" name="password" placeholder="Password"  />
+    <input type="text" name="email" placeholder="E-Mail"  />
+    <button>Sign Up</button>
+</form>
+    
+</body>
+</html>
